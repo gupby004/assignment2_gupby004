d55edaa3e224615e16e42b4eb64bc27992c2d4ba
gupby004 <gupby004@mymail.unisa.edu.au>
2023-11-16 16:49:28 +1030

    Reagent abstract class is created.

---
+++ ass2main_code.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Reagent(ABC):
+    def __init__(self, name, potency):
+        self.__name = name
+        self.__potency = potency
+
+    @abstractmethod
+    def refine(self):
+        pass
+
+    def getName(self):
+        return self.__name
+
+    def getPotency(self):
+        return self.__potency
+
+    def setPotency(self, new_potency):
+        self.__potency = new_potency
