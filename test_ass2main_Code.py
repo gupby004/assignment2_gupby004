49dc20a4dc8e4c53a02efb554518144adda208b0
gupby004 <gupby004@mymail.unisa.edu.au>
2023-11-17 13:24:15 +1030

    Testing herb class methods

--- test_ass2main_Code.py
+++ test_ass2main_Code.py
@@ -6,3 +6,26 @@ StudentID: 110395898
 EmailID: gupby004@mymail.unisa.edu.au
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
+
+import pytest
+from ass2main_code import * 
+
+"""
+Testing the methods of the herb class.
+"""
+def testHerbClass():
+    herb = Herb("Cadantine", 1.5)
+    assert herb.getName() == "Cadantine"
+    assert herb.getPotency() == 1.5
+    assert herb.isGrimy() is True
+
+def testHerbCreation():
+    herb = Herb("Cadantine", 1.5)
+    herb.refine()
+    assert herb.isGrimy() is False
+    assert herb.getPotency() == 1.5 * 2.5
+
+
+
+if __name__ == "__main__":
+    pytest.main()
