9b26a73129eef7396e4474ffc8a5eebf61b13020
gupby004 <gupby004@mymail.unisa.edu.au>
2023-11-17 12:56:31 +1030

    Details

---
+++ test_ass2main_Code.py
@@ -0,0 +1,8 @@
+'''
+File: ass2main_code.py
+Description: This file is about the UML diagram, Alchemist who has a laboratory and drinks potions. There are numerous types of reagents and potions.
+Author: Bhavya Gupta
+StudentID: 110395898
+EmailID: gupby004@mymail.unisa.edu.au
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
